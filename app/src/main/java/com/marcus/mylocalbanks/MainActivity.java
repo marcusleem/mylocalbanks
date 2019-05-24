fa4d34ab46a861d1895fb5f429542e5ecb4f8593
18037942 <18037942@rp.edu.sg>
2019-05-24 15:18:48 +0800

    submission2

--- app/src/main/java/com/marcus/mylocalbanks/MainActivity.java
+++ app/src/main/java/com/marcus/mylocalbanks/MainActivity.java
@@ -4,11 +4,11 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.ContextMenu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -31,86 +31,56 @@ public class MainActivity extends AppCompatActivity {
         registerForContextMenu(btnOCBC);
         registerForContextMenu(btnUOB);
 
-        btnDBS.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View v){
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.dbs.com.sg"));
-                startActivity(intent);
-            }
-        });
-
-        btnUOB.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.ocbc.com"));
-                startActivity(intent);
-            }
-        });
-
-        btnDBS.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.uob.com.sg"));
-                startActivity(intent);
-            }
-    });
-
-        btnDBS.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + getString(R.string.DBStel)));
-                startActivity(intent);
-            }
-        });
-
-        btnDBS.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + getString(R.string.OCBCtel)));
-                startActivity(intent);
-            }
-        });
-
-        btnDBS.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + getString(R.string.UOBtel)));
-                startActivity(intent);
-            }
-        });
 
     }
+
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v,
                                     ContextMenu.ContextMenuInfo menuInfo) {
         getMenuInflater().inflate(R.menu.menu, menu);
         super.onCreateContextMenu(menu, v, menuInfo);
 
-        if(v.getId()== R.id.btnDBS){
+        if (v.getId() == R.id.btnDBS) {
             mode = 1;
-        } else if (v.getId()== R.id.btnOCBC){
+        } else if (v.getId() == R.id.btnOCBC) {
             mode = 2;
-        }else{
+        } else {
             mode = 3;
         }
+        Log.d("TEST", "" + v.getId() + " " + mode);
+
 
     }
 
     @Override
     public boolean onContextItemSelected(MenuItem item) {
-        if (item.getItemId() == 0) { //check whether the selected menu item ID is 0
+        if (item.getItemId() == R.id.view) { //check whether the selected menu item ID is 0
             //code for action
-            Toast.makeText(MainActivity.this, "Website is chosen", Toast.LENGTH_SHORT).show();
+            if (mode == 1) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.dbs.com.sg"));
+                startActivity(intent);
+            } else if (mode == 2) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.ocbc.com"));
+                startActivity(intent);
+            } else {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.uob.com.sg"));
+                startActivity(intent);
+            }
+        } else {
+            if (mode == 1) {
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + getString(R.string.DBStel)));
+                startActivity(intent);
+            } else if (mode == 2) {
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + getString(R.string.OCBCtel)));
+                startActivity(intent);
+            } else {
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + getString(R.string.UOBtel)));
+                startActivity(intent);
+            }
 
-            return true; //menu item successfully handled
-        } else if (item.getItemId() == 1) { //check if the selected menu item ID is 1
-            //code for action
-            Toast.makeText(MainActivity.this, "Contact the bank is chosen", Toast.LENGTH_SHORT).show();
-
-            return true;  //menu item successfully handled
         }
         return super.onContextItemSelected(item); //pass menu item to the superclass implementation
     }
-}
 
+    ;
+}
